3c1cf68b96b0dfb4d4d246ab6fe14de0d2547f2d
Janusz Wdowka <j.wdowka2000@gmail.com>
2023-10-08 13:58:38 +0200

    Zmiany ustawiania drowerPanelControllera

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/BaseAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/BaseAdapter.java
@@ -1,19 +1,19 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
+import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.powp.jobs2d.features.DrawerFeature;
-
-import javax.swing.*;
 
 /**
  * driver adapter to drawer with several bugs.
  */
 public class BaseAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	private DrawPanelController drawPanelController;
 
-	public BaseAdapter() {
+	public BaseAdapter(DrawPanelController drawPanelController) {
+		this.drawPanelController = drawPanelController;
 	}
 
 	@Override
@@ -28,7 +28,7 @@ public class BaseAdapter implements Job2dDriver {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		DrawerFeature.getDrawerController().drawLine(line);
+		drawPanelController.drawLine(line);
 		this.setPosition(x, y);
 	}
 
