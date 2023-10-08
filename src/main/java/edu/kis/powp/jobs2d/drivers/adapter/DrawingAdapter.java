80f2b672344cdd1dd0cd7f46f6ff2a3ef033fe39
Mati-IR <mpabich4@gmail.com>
2023-10-08 14:02:27 +0200

    Fixed drawing in incorrect window

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawingAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawingAdapter.java
@@ -4,6 +4,9 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+
+import javax.swing.*;
 
 /**
  * driver adapter to drawer with several bugs.
@@ -27,7 +30,7 @@ public class DrawingAdapter extends DrawPanelController implements Job2dDriver {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		DrawerFeature.getDrawerController().drawLine(line);
 		setPosition(x, y);
 	}
 
