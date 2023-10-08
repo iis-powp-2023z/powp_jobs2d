d2d9e195b6a89f3239d20d2a50e035a63ed68674
Mati-IR <mpabich4@gmail.com>
2023-10-08 17:43:13 +0200

    Implemented SpecialLineDrawerAdapter and added it to driver's list

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -9,6 +9,7 @@ import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.SpecialLineDrawerAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
@@ -44,6 +45,9 @@ public class TestJobs2dPatterns {
 
 		Job2dDriver testDriver = new LineDrawerAdapter();
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
+
+		Job2dDriver specialTestDriver = new SpecialLineDrawerAdapter();
+		DriverFeature.addDriver("Special line driver", specialTestDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
