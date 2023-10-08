3e272376d5ef7be08e5171b36ec78f17c6747a78
gruszm <mariuszgruszczynski11@gmail.com>
2023-10-08 13:33:42 +0200

    Drawing bug fixed; refactoring

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DummyDrawingAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DummyDrawingAdapter.java
@@ -8,10 +8,10 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class DummyDrawingAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public MyAdapter() {
+	public DummyDrawingAdapter() {
 		super();
 	}
 
@@ -26,6 +26,9 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
+
+		startX = x;
+		startY = y;
 
 		drawLine(line);
 	}
