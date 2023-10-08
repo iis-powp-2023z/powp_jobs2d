40e5a5b21837e519d08ac3018a8a5f4fa2bfac4b
Janusz Wdowka <j.wdowka2000@gmail.com>
2023-10-08 13:49:44 +0200

    Naprawa używania jednej instancji drawerController

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/BaseAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/BaseAdapter.java
@@ -1,21 +1,19 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
-import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 import javax.swing.*;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class BaseAdapter extends DrawPanelController implements Job2dDriver {
+public class BaseAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public BaseAdapter(JPanel panel) {
-		//super();
-		initialize(panel);
+	public BaseAdapter() {
 	}
 
 	@Override
@@ -30,7 +28,7 @@ public class BaseAdapter extends DrawPanelController implements Job2dDriver {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		DrawerFeature.getDrawerController().drawLine(line);
 		this.setPosition(x, y);
 	}
 
--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -39,7 +39,7 @@ public class TestJobs2dPatterns {
 		DriverFeature.addDriver("Logger Driver", loggerDriver);
 		DriverFeature.getDriverManager().setCurrentDriver(loggerDriver);
 
-		Job2dDriver testDriver = new BaseAdapter(application.getFreePanel());
+		Job2dDriver testDriver = new BaseAdapter();
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
 
 		DriverFeature.updateDriverInfo();
