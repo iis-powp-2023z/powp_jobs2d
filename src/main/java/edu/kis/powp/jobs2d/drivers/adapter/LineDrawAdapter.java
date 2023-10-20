99d17531e1ab9d9e3b91d1e3383a18cf496ba848
maresyp <mattq.2000@icloud.com>
2023-10-20 12:24:35 +0200

    Add LineDrawAdapter and include it in test cases

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawAdapter.java
@@ -0,0 +1,30 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.panel.DrawPanelController;
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class LineDrawAdapter implements Job2dDriver {
+
+    private int startX = 0, startY = 0;
+    private final DrawPanelController drawPanelController;
+
+    public LineDrawAdapter(DrawPanelController drawerController) {
+        this.drawPanelController = drawerController;
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "LineDrawAdapter";
+    }
+}
--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -9,6 +9,7 @@ import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.Job2dDrawPanelAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDrawAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
@@ -42,6 +43,9 @@ public class TestJobs2dPatterns {
 
 		Job2dDriver testDriver = new Job2dDrawPanelAdapter(DrawerFeature.getDrawerController());
 		DriverFeature.addDriver("Drawer Simulator", testDriver);
+
+		Job2dDriver lineDriver = new LineDrawAdapter(DrawerFeature.getDrawerController());
+		DriverFeature.addDriver("Line Simulator", lineDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
