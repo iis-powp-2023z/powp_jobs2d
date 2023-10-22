dec3f58778c948a1cc52afca162c29c5843fb4b5
WPruska <236438@edu.p.lodz.pl>
2023-10-22 11:36:18 +0200

    hierarchia klas v2

--- src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
@@ -1,10 +1,16 @@
 package edu.kis.powp.jobs2d.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class OperateToCommand implements DriverCommand{
     private int x;
     private int y;
+    public OperateToCommand(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
     @Override
-    public void execute() {
+    public void execute(Job2dDriver driver) {
 
     }
 }
