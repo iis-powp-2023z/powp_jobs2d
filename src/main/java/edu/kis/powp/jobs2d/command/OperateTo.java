fc859597a55e3a3f13f29ff63673163065934f99
Mati-IR <mpabich4@gmail.com>
2023-10-22 12:24:24 +0200

    Empty Command driver and its commands

---
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateTo.java
@@ -0,0 +1,9 @@
+package edu.kis.powp.jobs2d.command;
+
+public class OperateTo implements DriverCommand{
+    private int x;
+    private int y;
+    @Override
+    public void execute() {
+    }
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPosition.java
@@ -0,0 +1,11 @@
+package edu.kis.powp.jobs2d.command;
+
+public class SetPosition implements DriverCommand{
+    private int x;
+    private int y;
+
+    @Override
+    public void execute() {
+
+    }
+}
