8952e66f575e75268204a3286ffdefba93ffb865
Janusz Wdowka <j.wdowka2000@gmail.com>
2023-10-22 11:24:54 +0200

    3.1.1 zmiana podejścia

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -5,8 +5,9 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 public class OperateToCommand implements DriverCommand {
     private int x;
     private int y;
+    private Job2dDriver job2dDriver;
     @Override
-    public void execute(Job2dDriver driver) {
+    public void execute() {
 
     }
 }
