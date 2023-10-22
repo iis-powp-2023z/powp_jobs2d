45f6eb2962d437cb4bfce9d47c503f487629408f
WPruska <236438@edu.p.lodz.pl>
2023-10-22 11:00:41 +0200

    hierarchia klas

---
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
@@ -0,0 +1,10 @@
+package edu.kis.powp.jobs2d.command;
+
+public class OperateToCommand implements DriverCommand{
+    private int x;
+    private int y;
+    @Override
+    public void execute() {
+
+    }
+}
