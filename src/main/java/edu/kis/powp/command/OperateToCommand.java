c70f744114f79823f105a7ec85ca83e64d0f2a7b
Janusz Wdowka <j.wdowka2000@gmail.com>
2023-10-22 11:37:03 +0200

    3.1.2

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -6,8 +6,27 @@ public class OperateToCommand implements DriverCommand {
     private int x;
     private int y;
     private Job2dDriver job2dDriver;
+
+    public OperateToCommand(int x, int y, Job2dDriver job2dDriver) {
+        this.x = x;
+        this.y = y;
+        this.job2dDriver = job2dDriver;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setJob2dDriver(Job2dDriver job2dDriver) {
+        this.job2dDriver = job2dDriver;
+    }
+
     @Override
     public void execute() {
-
+        this.job2dDriver.operateTo(x, y);
     }
 }
--- src/main/java/edu/kis/powp/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -6,8 +6,27 @@ public class SetPositionCommand implements DriverCommand {
     private int x;
     private int y;
     private Job2dDriver job2dDriver;
+
+    public SetPositionCommand(int x, int y, Job2dDriver job2dDriver) {
+        this.x = x;
+        this.y = y;
+        this.job2dDriver = job2dDriver;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setJob2dDriver(Job2dDriver job2dDriver) {
+        this.job2dDriver = job2dDriver;
+    }
+
     @Override
     public void execute() {
-
+        this.job2dDriver.setPosition(x, y);
     }
 }
