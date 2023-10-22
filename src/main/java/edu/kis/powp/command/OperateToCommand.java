147c7aa213599ac03aadda2e70965551729e6872
Janusz Wdowka <j.wdowka2000@gmail.com>
2023-10-22 11:17:39 +0200

    3.1.1 Utworze klas i interfejsu Command

---
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -0,0 +1,12 @@
+package edu.kis.powp.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand {
+    private int x;
+    private int y;
+    @Override
+    public void execute(Job2dDriver driver) {
+
+    }
+}
