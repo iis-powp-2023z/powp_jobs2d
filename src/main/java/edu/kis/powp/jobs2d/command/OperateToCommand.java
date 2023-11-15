db791a76c25c78097359394db8ebcf993c57afe9
MrGrzesiek <78262537+MrGrzesiek@users.noreply.github.com>
2023-11-15 18:19:44 +0100

    naprawa implementacji

--- src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
@@ -6,8 +6,12 @@ public class OperateToCommand implements DriverCommand {
     private int x;
     private int y;
 
+    public OperateToCommand(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
     @Override
-    public void execute() {
-
+    public void execute(Job2dDriver driver) {
+        driver.operateTo(x,y);
     }
 }
--- src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -1,10 +1,18 @@
 package edu.kis.powp.jobs2d.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand{
     private int x;
     private int y;
-    @Override
-    public void execute(){
 
+    public SetPositionCommand(int x,int y){
+        this.x=x;
+        this.y=y;
+
+    }
+    @Override
+    public void execute(Job2dDriver driver) {
+        driver.setPosition(x,y);
     }
 }
