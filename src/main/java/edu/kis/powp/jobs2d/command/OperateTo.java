07fd8cacf75cc8dd5b849307e429578626af3602
Mati-IR <mpabich4@gmail.com>
2023-11-26 13:09:25 +0100

    Update operations

--- src/main/java/edu/kis/powp/jobs2d/command/OperateTo.java
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateTo.java
@@ -6,7 +6,12 @@ public class OperateTo implements DriverCommand{
     private int x;
     private int y;
 
+    public OperateTo(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
     @Override
     public void execute(Job2dDriver driver) {
+        driver.operateTo(x,y);
     }
 }
--- src/main/java/edu/kis/powp/jobs2d/command/SetPosition.java
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPosition.java
@@ -6,8 +6,13 @@ public class SetPosition implements DriverCommand{
     private int x;
     private int y;
 
+    public SetPosition(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
     @Override
     public void execute(Job2dDriver driver) {
-
+        driver.setPosition(x,y);
     }
 }
